697829b297deb87057575531e305fdca5999b8b5
jalal Arif <jalalharfjalal@gmail.com>
2020-05-16 19:04:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 
 
 // // Code your solutions in this file
-function writeCards(names , eventName){
+function writeCards(names , eventNames){
   let newArr = []
   for(let i=0; i<names.length;i++){
-    newArr.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    newArr.push(`Thank you, ${names[i]}, for the wonderful ${eventNames}your gift!`);
   }
   return newArr;
 }
 
 writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+
 
 
 function countDown(num){

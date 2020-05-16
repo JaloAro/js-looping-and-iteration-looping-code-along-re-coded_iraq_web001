0c11f7f38757a3ab3dba9931e59ae707fefebbe8
jalal Arif <jalalharfjalal@gmail.com>
2020-05-16 19:00:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-
-
-function writeCards(cards,gifts) {
-  const array = [];
+function writeCards(cards,gift) {
+  const newArr = [];
   for (let i = 0; i < cards.length; i++) {
-    array.push(`Thanks, ${cards[i]}, for the wonderful ${gifts}your gift!`);
+    newArr.push(`Thank you, ${cards[i]}, for the wonderful ${gift} gift!`);
   }
 
-  return array;
+  return newArr;
 }
 
 writeCards([ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise');
-
 
 
 function countDown(num) {
@@ -25,3 +22,32 @@ while(num >= 0){
 countDown(10);
 
 
+
+
+
+
+
+
+
+
+// // Code your solutions in this file
+function writeCards(names , eventName){
+  let newArr = []
+  for(let i=0; i<names.length;i++){
+    newArr.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return newArr;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+
+function countDown(num){
+  
+  while(num>=0){
+    console.log(num);
+    num --;
+  }
+}
+
+countDown(10);
